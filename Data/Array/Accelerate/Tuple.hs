483dd93672b2399dcea09cfa0a2082ea28f611fa
Manuel M T Chakravarty <chak@cse.unsw.edu.au>
2009-10-03 22:29:38 +1000

    Add Tuple.hs
    Ignore-this: 322dde71d53c6e74095c54eea09c33e3
    
    darcs-hash:20091003122938-6295e-7f74e0d2ba1c22c0199b22fe708303d9286be0e6.gz

---
+++ Data/Array/Accelerate/Tuple.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE GADTs, TypeFamilies, FlexibleInstances #-}
+
+-- |Embedded array processing language: tuple representation
+--
+--  Copyright (c) 2009 Manuel M T Chakravarty, Gabriele Keller, Sean Lee
+--
+--  License: BSD3
+--
+--- Description ---------------------------------------------------------------
+--
+--  Our representation of tuples are heterogenous snoc lists, which are typed 
+--  by type lists, where '()' and '(,)' are type-level nil and snoc,
+--  respectively.  The components may only be drawn from types that can be
+--  used as array elements.
+
+module Data.Array.Accelerate.Tuple (
+
+  -- * Tuple representation
+  Tuple(..), TupleIdx(..), IsTuple(..)
+  
+) where
+
+-- friends
+import Data.Array.Accelerate.Array.Sugar 
+
+
+-- Tuple representation
+-- --------------------
+
+-- |We represent tuples as heterogenous lists, typed by a type list.
+--
+data Tuple c t where
+  NilTup  ::                               Tuple c ()
+  SnocTup :: Elem t => Tuple c s -> c t -> Tuple c (s, t)
+
+-- |Type-safe projection indicies for tuples.
+--
+-- NB: We index tuples by starting to count from the *right*!
+--
+data TupleIdx t e where
+  ZeroTupIdx ::                 TupleIdx (t, s) s
+  SuccTupIdx :: TupleIdx t e -> TupleIdx (t, s) e
+
+-- |Conversion between surface n-tuples and our tuple representation.
+--
+class IsTuple tup where
+  type TupleRepr tup
+  fromTuple :: tup -> TupleRepr tup
+  toTuple   :: TupleRepr tup -> tup
+
+instance IsTuple (a, b) where
+  type TupleRepr (a, b) = (((), a), b)
+  fromTuple (x, y)      = (((), x), y)
+  toTuple (((), x), y)  = (x, y)
+            
+instance IsTuple (a, b, c) where
+  type TupleRepr (a, b, c)  = (TupleRepr (a, b), c)
+  fromTuple (x, y, z)       = ((((), x), y), z)
+  toTuple ((((), x), y), z) = (x, y, z)
+
+instance IsTuple (a, b, c, d) where
+  type TupleRepr (a, b, c, d)    = (TupleRepr (a, b, c), d)
+  fromTuple (x, y, z, v)         = (((((), x), y), z), v)
+  toTuple (((((), x), y), z), v) = (x, y, z, v)
+
+instance IsTuple (a, b, c, d, e) where
+  type TupleRepr (a, b, c, d, e)      = (TupleRepr (a, b, c, d), e)
+  fromTuple (x, y, z, v, w)           = ((((((), x), y), z), v), w)
+  toTuple ((((((), x), y), z), v), w) = (x, y, z, v, w)
