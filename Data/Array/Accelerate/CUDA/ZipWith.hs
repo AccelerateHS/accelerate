a95026d5943ada4963fa782753e4b92bf1dd416b
Sean Lee <seanl@cse.unsw.edu.au>
2010-03-02 16:51:56 +1100

    Removed the host code generation bit from D/A/A/C/ZipWith.hs as the CUDA binding, instead of generated host code, is used to launch kernels.
    Ignore-this: bfb6cd8bd55fef5b9f283d5d7744a3f5
    
    darcs-hash:20100302055156-879de-73ceb8a4bb43fba1414e520cda185e5b06359e22.gz

--- Data/Array/Accelerate/CUDA/ZipWith.hs
+++ Data/Array/Accelerate/CUDA/ZipWith.hs
@@ -3,7 +3,6 @@ module Data.Array.Accelerate.CUDA.ZipWith (zipWithGen, zipWithDot) where
 import Data.Array.Accelerate.CUDA.Scalar
 import Data.Array.Accelerate.CUDA.Syntax
 import Data.Array.Accelerate.CUDA.DeviceCodeGen
-import Data.Array.Accelerate.CUDA.HostCodeGen
 
 ------------------------------------------------------------------------------
 -- CodeGen
@@ -11,44 +10,21 @@ import Data.Array.Accelerate.CUDA.HostCodeGen
 zipWithGen :: String -> Scalar -> IO ()
 zipWithGen progName scalar =
   if length (params scalar) == 2 && not (null $ comp scalar)
-    then do
+    then
       writeFile (progName ++ ".cu") (show $ deviceCode progName scalar)
-      writeFile (progName ++ ".cpp") (show impl)
-      writeFile (progName ++ ".h") (show header)
-    else error
-      "ZipWith requires a binary function (a -> b -> c) as the scalar."
-  where
-    (header, impl) = hostCode progName scalar
+    else
+      error "ZipWith requires a binary function (a -> b -> c) as the scalar."
 
 zipWithDot :: String -> Scalar -> IO ()
 zipWithDot progName scalar =
   if length (params scalar) == 2 && not (null $ comp scalar)
-    then do
+    then
       writeFile (progName ++ ".cu.dot") (dot $ deviceCode progName scalar)
-      writeFile (progName ++ ".cpp.dot") (dot impl)
-      writeFile (progName ++ ".cpp.dot") (dot header)
-    else error
-      "ZipWith requires a binary function (a -> b -> c) as the scalar."
-  where
-    (header, impl) = hostCode progName scalar
+    else
+      error "ZipWith requires a binary function (a -> b -> c) as the scalar."
 
 ------------------------------------------------------------------------------
 -- Device Code
 ------------------------------------------------------------------------------
 deviceCode :: String -> Scalar -> TransUnit
 deviceCode = Data.Array.Accelerate.CUDA.DeviceCodeGen.zipWith
-
-------------------------------------------------------------------------------
--- HostCode
-------------------------------------------------------------------------------
-hostCode :: String -> Scalar -> (TransUnit, TransUnit)
-hostCode progName scalar = hostCodeGen $ HostCodeConfig
-  { progName = progName
-  , ctaSize = 64
-  , nPerThread = 8
-  , sharedMemSize = Nothing
-  , inParams = [Array (varName ++ "s") ty | (ty, varName) <- params scalar]
-  , outParam = Array "out" (outTy scalar)
-  , scalar = Nothing
-  , op = Data.Array.Accelerate.CUDA.HostCodeGen.Map
-    {kernel = Data.Array.Accelerate.CUDA.DeviceCodeGen.ZipWith}}
