2a2553257b79db1441facfb2e146d6d90bbeb21f
Sean Lee <seanl@cse.unsw.edu.au>
2010-03-02 16:58:39 +1100

    Extra fields have been added to CGState to keep track of the memory use and the code compilation.
    Ignore-this: 4f6d0ee66e8b0963ce6c897919c530
    
    darcs-hash:20100302055839-879de-ca82bc4997822de487a9f9b93f4d080963019d07.gz

--- Data/Array/Accelerate/CUDA/Monad.hs
+++ Data/Array/Accelerate/CUDA/Monad.hs
@@ -2,11 +2,31 @@
 -- state, including the unique suffix used in the code generator.
 
 module Data.Array.Accelerate.CUDA.Monad (
-  CGIO, CGState(..)
+  CGIO, CGState(..), OperationMapValue(..)
 ) where
 
 import Control.Monad.State
-
-data CGState = CGState { uniqueID :: Int }
+import Data.Map
+import Foreign.Ptr
+import System.Posix.Types
 
 type CGIO = StateT CGState IO
+
+data OperationMapValue
+  = OperationValue
+    { compilerPID :: ProcessID  -- the PID of the compiler
+    , progName    :: String     -- generated CUDA kernel name
+    , devicePtr   :: WordPtr}   -- device pointer for the output
+  | MemoryEntry
+    --{ hostPtr     :: WordPtr    -- host pointer pinned by CUDA driver API
+    { devicePtr   :: WordPtr}   -- device pointer
+  deriving Show
+
+data CGState = CGState
+  { uniqueID         :: Int
+  , dataTransferHtoD :: Int
+  , mapMap           :: Map String  OperationMapValue
+  , zipWithMap       :: Map String  OperationMapValue
+  , foldMap          :: Map String  OperationMapValue
+  , useMap           :: Map WordPtr OperationMapValue} -- accelerate array pointer is the key value
+  deriving Show
