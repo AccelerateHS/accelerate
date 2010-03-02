756b09cbe7407dca204fdb88e0ddc7c1104e1db4
Sean Lee <seanl@cse.unsw.edu.au>
2010-03-02 16:53:45 +1100

    Removed the host code generation bit from D/A/A/C/Map.hs  as the CUDA binding, instead of generated host code, is used to launch kernels.
    Ignore-this: f3de3ec41aeba06d4b38a71600f00d97
    
    darcs-hash:20100302055345-879de-bacb51faab8bb4a9ad02de49a0df4bd96d6af623.gz

--- Data/Array/Accelerate/CUDA/Map.hs
+++ Data/Array/Accelerate/CUDA/Map.hs
@@ -3,7 +3,6 @@ module Data.Array.Accelerate.CUDA.Map (mapGen, mapDot) where
 import Data.Array.Accelerate.CUDA.Scalar
 import Data.Array.Accelerate.CUDA.Syntax
 import Data.Array.Accelerate.CUDA.DeviceCodeGen
-import Data.Array.Accelerate.CUDA.HostCodeGen
 
 ------------------------------------------------------------------------------
 -- CodeGen
@@ -11,44 +10,21 @@ import Data.Array.Accelerate.CUDA.HostCodeGen
 mapGen :: String -> Scalar -> IO ()
 mapGen progName scalar =
   if length (params scalar) == 1 && not (null $ comp scalar)
-    then do
+    then
       writeFile (progName ++ ".cu") (show $ deviceCode progName scalar)
-      writeFile (progName ++ ".cpp") (show impl)
-      writeFile (progName ++ ".h") (show header)
-    else error
-      "Map requires a unary function (a -> b) as the scalar."
-  where
-    (header, impl) = hostCode progName scalar
+    else
+      error "Map requires a unary function (a -> b) as the scalar."
 
 mapDot :: String -> Scalar -> IO ()
 mapDot progName scalar = do
   if length (params scalar) == 1 && not (null $ comp scalar)
-    then do
+    then
       writeFile (progName ++ ".cu.dot") (dot $ deviceCode progName scalar)
-      writeFile (progName ++ ".cpp.dot") (dot impl)
-      writeFile (progName ++ ".h.dot") (dot header)
-    else error
-      "Map requires a unary function (a -> b) as the scalar."
-  where
-    (header, impl) = hostCode progName scalar
+    else
+      error "Map requires a unary function (a -> b) as the scalar."
 
 ------------------------------------------------------------------------------
 -- Device Code
 ------------------------------------------------------------------------------
 deviceCode :: String -> Scalar -> TransUnit
 deviceCode = Data.Array.Accelerate.CUDA.DeviceCodeGen.map
-
-------------------------------------------------------------------------------
--- Host Code
-------------------------------------------------------------------------------
-hostCode :: String -> Scalar -> (TransUnit, TransUnit)
-hostCode progName scalar = hostCodeGen $ HostCodeConfig
-  { progName = progName
-  , ctaSize = 64
-  , nPerThread = 8
-  , sharedMemSize = Nothing
-  , inParams = [Array (varName ++ "s") ty | (ty, varName) <- params scalar]
-  , outParam = Array "out" (outTy scalar)
-  , scalar = Nothing
-  , op = Data.Array.Accelerate.CUDA.HostCodeGen.Map
-    {kernel = Data.Array.Accelerate.CUDA.DeviceCodeGen.Map}}
