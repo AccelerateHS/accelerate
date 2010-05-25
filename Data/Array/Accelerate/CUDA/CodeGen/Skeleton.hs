b9d5e008d47cb9088d86dde8bf34466199a1149b
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2010-05-25 23:31:17 +1000

    meta-module to export all skeleton ASTs
    Ignore-this: f153a397bdd37fd6ab29349f51421e80
    
    darcs-hash:20100525133117-dcabc-b3f1fa95d8bcfd5618b634eeeb6753a86cb612b4.gz

---
+++ Data/Array/Accelerate/CUDA/CodeGen/Skeleton.hs
@@ -0,0 +1,18 @@
+-- |
+-- Module      : Data.Array.Accelerate.CUDA.CodeGen.Skeleton
+-- Copyright   : [2008..2009] Manuel M T Chakravarty, Gabriele Keller, Sean Lee
+-- License     : BSD3
+--
+-- Maintainer  : Manuel M T Chakravarty <chak@cse.unsw.edu.au>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+--
+
+module Data.Array.Accelerate.CUDA.CodeGen.Skeleton
+  (
+    module Data.Array.Accelerate.CUDA.CodeGen.Map
+  )
+  where
+
+import Data.Array.Accelerate.CUDA.CodeGen.Map
+
