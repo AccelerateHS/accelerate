63c25280d1204a69ea4841739b75244f9de1109b
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2010-06-22 19:36:59 +1000

    overloaded functions for indexing multidimensional shapes
    Ignore-this: ff6e16226b3eea90051d518cfd617a6e
    
    darcs-hash:20100622093659-dcabc-bb472f3595f322fe53df76f82a8f906a9efacc2d.gz

--- cubits/accelerate_cuda_extras.h
+++ cubits/accelerate_cuda_extras.h
@@ -25,7 +25,7 @@
  *
  * FIXME: We need extra code generation support for 64-bit types
  */
-#ifdef __cplusplus
+#if defined(__cplusplus) && defined(__CUDACC__)
 
 typedef texture<uint32_t, 1> TexWord;
 typedef texture<uint32_t, 2> TexWord64;
@@ -48,12 +48,10 @@ typedef void* TexWord;
 typedef void* TexWord32;
 typedef void* TexWord16;
 typedef void* TexWord8;
-
 typedef void* TexInt;
 typedef void* TexInt32;
 typedef void* TexInt16;
 typedef void* TexInt8;
-
 typedef void* TexFloat;
 typedef void* TexDouble;
 
@@ -65,8 +63,69 @@ void* tex3D(const void*, const float, const float, const float);
 #endif
 
 /* -----------------------------------------------------------------------------
- * Indices
+ * Shapes
  * -------------------------------------------------------------------------- */
+
+typedef unsigned int             Ix;
+typedef Ix                       DIM1;
+typedef struct { Ix a,b; }       DIM2;
+typedef struct { Ix a,b,c; }     DIM3;
+typedef struct { Ix a,b,c,d; }   DIM4;
+typedef struct { Ix a,b,c,d,e; } DIM5;
+
+#ifdef __cplusplus
+
+static __inline__ __device__ int dim(DIM1 sh) { return 1; }
+static __inline__ __device__ int dim(DIM2 sh) { return 2; }
+static __inline__ __device__ int dim(DIM3 sh) { return 3; }
+static __inline__ __device__ int dim(DIM4 sh) { return 4; }
+static __inline__ __device__ int dim(DIM5 sh) { return 5; }
+
+static __inline__ __device__ int size(DIM1 sh) { return sh; }
+static __inline__ __device__ int size(DIM2 sh) { return sh.a * sh.b; }
+static __inline__ __device__ int size(DIM3 sh) { return sh.a * sh.b * sh.c; }
+static __inline__ __device__ int size(DIM4 sh) { return sh.a * sh.b * sh.c * sh.d; }
+static __inline__ __device__ int size(DIM5 sh) { return sh.a * sh.b * sh.c * sh.d * sh.e; }
+
+static __inline__ __device__ int index(DIM1 sh, DIM1 ix)
+{
+    return ix;
+}
+
+static __inline__ __device__ int index(DIM2 sh, DIM2 ix)
+{
+    DIM1 sh_ = sh.a;
+    DIM1 ix_ = ix.a;
+    return index(sh_, ix_) + ix.b * size(sh_);
+}
+
+static __inline__ __device__ int index(DIM3 sh, DIM3 ix)
+{
+    DIM2 sh_ = { sh.a, sh.b };
+    DIM2 ix_ = { ix.a, ix.b };
+    return index(sh_, ix_) + ix.c * size(sh_);
+}
+
+static __inline__ __device__ int index(DIM4 sh, DIM4 ix)
+{
+    DIM3 sh_ = { sh.a, sh.b, sh.c };
+    DIM3 ix_ = { ix.a, ix.b, ix.c };
+    return index(sh_, ix_) + ix.d * size(sh_);
+}
+
+static __inline__ __device__ int index(DIM5 sh, DIM5 ix)
+{
+    DIM4 sh_ = { sh.a, sh.b, sh.c, sh.d };
+    DIM4 ix_ = { ix.a, ix.b, ix.c, ix.d };
+    return index(sh_, ix_) + ix.e * size(sh_);
+}
+
+
+#else
+int dim(Ix sh);
+int size(Ix sh);
+int index(Ix sh, Ix ix);
+#endif
 
 
 /* -----------------------------------------------------------------------------
