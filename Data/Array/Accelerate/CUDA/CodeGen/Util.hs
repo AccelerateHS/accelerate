9f18734a2cea82e4f3a39e447b32257efb116ea2
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2010-08-30 10:26:37 +1000

    amend last patch
    Ignore-this: 126d3f66436c3526f0df45ec2eb0247
    
    darcs-hash:20100830002637-dcabc-44d7f53b9759c5fe6555ee907db7243b203e98b1.gz

--- Data/Array/Accelerate/CUDA/CodeGen/Util.hs
+++ Data/Array/Accelerate/CUDA/CodeGen/Util.hs
@@ -62,7 +62,7 @@ mkTypedef var ptr ty =
     internalNode
 
 mkInitList :: [CExpr] -> CInit
-mkInitList []  = mkInitList [CConst (CIntConst (cInteger 0) internalNode)]
+mkInitList []  = CInitExpr (CConst (CIntConst (cInteger 0) internalNode)) internalNode
 mkInitList [x] = CInitExpr x internalNode
 mkInitList xs  = CInitList (map (\e -> ([],CInitExpr e internalNode)) xs) internalNode
 
