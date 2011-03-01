bcae8e681779695983ff6fdd8f9e8e13ef4c7242
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2011-03-01 16:32:56 +1100

    move and disable bit-rotted quickcheck test-suite
    Ignore-this: 2b3b8973c66da6bbaa6f06101d0329c3
    
    darcs-hash:20110301053256-dcabc-0436c0db253f956ea38f9597f3805d4b97e412af.gz

---
+++ src/broken/src/Function.hs
@@ -0,0 +1,24 @@
+
+module Function (filter) where
+
+import Prelude hiding (scanl, map, filter, length)
+import Data.Array.Accelerate
+
+-- stolen from examples/simple/Filter.hs
+--
+filter :: Elem a
+       => (Exp a -> Exp Bool)
+       -> Acc (Vector a)
+       -> Acc (Vector a)
+filter p arr
+  = let flags               = map (boolToInt . p) arr
+        (targetIdx, length) = scanl (+) 0 flags
+        arr'                = backpermute (length!(constant ())) id arr
+    in
+    permute const arr' (\ix -> flags!ix ==* 0 ? (ignore, targetIdx!ix)) arr
+    -- FIXME: This is abusing 'permute' in that the first two arguments are
+    --        only justified because we know the permutation function will
+    --        write to each location in the target exactly once.
+    --        Instead, we should have a primitive that directly encodes the
+    --        compaction pattern of the permutation function.
+
