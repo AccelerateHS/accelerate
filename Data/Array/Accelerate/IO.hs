8cc6528de5e65eeb00a92e4739dd40dfec8c8879
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2011-03-05 20:35:39 +1100

    move summary comment about block-copy/IO stuff
    Ignore-this: 768651a690bc759137eb8824d7af642a
    
    darcs-hash:20110305093539-dcabc-f6fc3b0ef7b9bc93bcf2f81595907e0d4fc65c07.gz

--- Data/Array/Accelerate/IO.hs
+++ Data/Array/Accelerate/IO.hs
@@ -7,6 +7,18 @@
 -- Stability   : experimental
 -- Portability : non-portable (GHC extensions)
 --
+-- This module provides functions for efficient block copies of primitive arrays
+-- (i.e. one dimensional, in row-major order in contiguous memory) to Accelerate
+-- Arrays.
+--
+-- You should only use this module if you really know what you are doing.
+-- Potential pitfalls include:
+--
+--   * copying from memory your program doesn't have access to (e.g. it may be
+--     unallocated or not enough memory is allocated)
+--
+--   * memory alignment errors
+--
 
 module Data.Array.Accelerate.IO (
 
