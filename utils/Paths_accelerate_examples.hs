a00a4f8e8a7a28353fbf5420d506e83b812fc53f
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2011-11-14 10:50:31 +1100

    provide Paths_accelerate_examples.hs for GHCi
    rather than relying on 'cabal configure' having being run so that the auto generated version is present.

---
+++ utils/Paths_accelerate_examples.hs
@@ -0,0 +1,11 @@
+-- Sample module for use under GHCi. The actual implementation is generated
+-- automatically by cabal.
+--
+
+module Paths_accelerate_examples where
+
+import Data.Version
+
+version :: Version
+version = Version {versionBranch = [0,1,0,0], versionTags = []}
+
