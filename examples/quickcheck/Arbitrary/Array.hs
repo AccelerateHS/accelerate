476f94cc4a842e8d220dbca7aae6a0633ee9514d
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2012-03-07 17:47:05 +1100

    don't include duplicate indices when shrinking

--- examples/quickcheck/Arbitrary/Array.hs
+++ examples/quickcheck/Arbitrary/Array.hs
@@ -6,6 +6,7 @@ module Arbitrary.Array where
 
 import Arbitrary.Shape
 
+import Data.List
 import Test.QuickCheck
 import Data.Array.Accelerate.Array.Sugar
 
@@ -27,7 +28,7 @@ instance (Elt e, Arbitrary e) => Arbitrary (Array DIM1 e) where
   --
   shrink arr =
     let (Z :. n)        = shape arr
-        indices         = [ map (Z:.) sz | sz <- shrink [0 .. n-1] ]
+        indices         = [ map (Z:.) (nub sz) | sz <- shrink [0 .. n-1] ]
     in
     [ fromList (Z :. length sl) (map (arr!) sl) | sl <- indices ]
 
@@ -41,9 +42,9 @@ instance (Elt e, Arbitrary e) => Arbitrary (Array DIM2 e) where
   shrink arr =
     let (Z :. width :. height)   = shape arr
     in
-    [ fromList (Z :. length slx :. length sly) [ arr ! (Z:.x:.y) | x <- slx, y <- sly ]
+    [ fromList (Z :. length slx :. length sly) [ arr ! (Z:.x:.y) | x <- nub slx, y <- nub sly ]
         | slx <- shrink [0 .. width  - 1]
         , sly <- shrink [0 .. height - 1]
 --        , null slx == null sly  -- only (Z:.0:.0) and non-empty arrays
-        ]
+    ]
 
