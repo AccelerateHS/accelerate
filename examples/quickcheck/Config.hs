08918f7deb62120cc8f0dd063abaa2fabf696de1
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2012-03-19 16:35:23 +1100

    display some usage info at the start of a run

--- examples/quickcheck/Config.hs
+++ examples/quickcheck/Config.hs
@@ -1,22 +1,31 @@
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE CPP #-}
 
 module Config (
 
-  Options, optBackend,
-  run, processArgs
+  -- options & test configuration
+  Options,
+  processArgs,
+  optBackend, double, float, int64, int32, int16, int8,
+
+  -- running tests
+  run, run1
 
 ) where
 
 import Data.Char
 import Data.List
 import Data.Label
+import Control.Monad
 import Test.Framework
-import System.Console.GetOpt
+import System.Console.GetOpt                            ( OptDescr(..), ArgDescr(..) )
 
 import Data.Array.Accelerate                            ( Arrays, Acc )
 import qualified Data.Array.Accelerate.Interpreter      as Interp
+
 #ifdef ACCELERATE_CUDA_BACKEND
+import qualified Foreign.CUDA.Driver                    as CUDA
 import qualified Data.Array.Accelerate.CUDA             as CUDA
 #endif
 
@@ -29,15 +38,32 @@ data Backend = Interpreter
 
 data Options = Options
   {
-    _optBackend         :: Backend
+    _optBackend         :: !Backend,
+    _double             :: !Bool,
+    _float              :: !Bool,
+    _int64              :: !Bool,
+    _int32              :: !Bool,
+    _int16              :: !Bool,
+    _int8               :: !Bool
   }
   deriving Show
 
 $( mkLabels [''Options] )
 
+defaultOptions :: Options
+defaultOptions =  Options
+  { _optBackend         = maxBound
+  , _double             = True
+  , _float              = True
+  , _int64              = True
+  , _int32              = True
+  , _int16              = True
+  , _int8               = True
+  }
 
-options :: [OptDescr (Options -> Options)]
-options =
+
+backends :: [OptDescr (Options -> Options)]
+backends =
   [ Option [] ["interpreter"]           (NoArg (set optBackend Interpreter))    "reference implementation (sequential)"
 #ifdef ACCELERATE_CUDA_BACKEND
   , Option [] ["cuda"]                  (NoArg (set optBackend CUDA))           "implementation for NVIDIA GPUs (parallel)"
@@ -52,16 +78,73 @@ run opts = case _optBackend opts of
   CUDA          -> CUDA.run
 #endif
 
+run1 :: (Arrays a, Arrays b) => Options -> (Acc a -> Acc b) -> a -> b
+run1 opts f = case _optBackend opts of
+  Interpreter   -> head . Interp.stream f . return
+#ifdef ACCELERATE_CUDA_BACKEND
+  CUDA          -> CUDA.run1 f
+#endif
+
+
+-- Display a very basic usage message info: specifically, list the available
+-- backends and highlight the active one.
+--
+usageInfo :: Options -> String
+usageInfo opts = unlines (header : table)
+  where
+    active this         = if this == map toLower (show $ get optBackend opts) then "*" else ""
+    (ss,bs,ds)          = unzip3 $ map (\(b,d) -> (active b, b, d)) $ concatMap extract backends
+    table               = zipWith3 paste (sameLen ss) (sameLen bs) ds
+    paste x y z         = "  " ++ x ++ "  " ++ y ++ "  " ++ z
+    sameLen xs          = flushLeft ((maximum . map length) xs) xs
+    flushLeft n xs      = [ take n (x ++ repeat ' ') | x <- xs ]
+    --
+    extract (Option _ los _ descr) =
+      let losFmt  = intercalate ", " los
+      in  case lines descr of
+            []          -> [(losFmt, "")]
+            (x:xs)      -> (losFmt, x) : [ ("",x') | x' <- xs ]
+    --
+    header              = intercalate "\n" $
+      [ "accelerate-quickcheck (c) 2012 The Accelerate Team"
+      , ""
+      , "Usage: accelerate-quickcheck [BACKEND] [OPTIONS]"
+      , ""
+      , "Available backends:"
+      ]
+
+-- Once the backend has been selected, we might need to enable or disable
+-- certain classes of tests.
+--
+-- CUDA: double precision is only supported on certain kinds of hardware. We
+--       can't get the exact device the backend will choose to run on, but we do
+--       know that it will pick the most capable device available.
+--
+configureBackend :: Options -> IO Options
+configureBackend opts = case _optBackend opts of
+  Interpreter   -> return opts
+#ifdef ACCELERATE_CUDA_BACKEND
+  CUDA          -> do
+    CUDA.initialise []
+    n           <- CUDA.count
+    devs        <- mapM CUDA.device [0 .. n-1]
+    props       <- mapM CUDA.props devs
+    return      $! opts { _double = any (\dev -> CUDA.computeCapability dev >= 1.3) props }
+#endif
+
+
 processArgs :: [String] -> IO (Options, RunnerOptions)
 processArgs argv = do
-  args  <- interpretArgs argv
-  case args of
+  args                  <- interpretArgs argv
+  (options, runner)     <- case args of
     Left msg            -> error msg
-    Right (opts, rest)  -> return $! (foldl parse1 defaults rest, opts)
+    Right (opts, rest)  -> (,opts) `liftM` configureBackend (foldl parse1 defaultOptions rest)
   --
+  putStrLn      $ usageInfo options
+  return        $ (options, runner)
   where
-    defaults            = Options maxBound
-    parse1 opts x       = case filter (\(Option _ [f] _ _) -> map toLower x `isPrefixOf` f) options of
-                            [Option _ _ (NoArg go) _]   -> go opts
-                            _                           -> opts
+    parse1 opts x       =
+      case filter (\(Option _ [f] _ _) -> map toLower x `isPrefixOf` f) backends of
+        [Option _ _ (NoArg go) _]   -> go opts
+        _                           -> opts
 
