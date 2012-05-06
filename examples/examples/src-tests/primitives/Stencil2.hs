4f093deedb48cce71e7d4b9b41abd3a3e97ffabb
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2012-05-06 15:13:40 +1000

    fix IArray indexing in stencil2 test

--- examples/examples/src-tests/primitives/Stencil2.hs
+++ examples/examples/src-tests/primitives/Stencil2.hs
@@ -18,6 +18,43 @@ stencil2D2 ((_,t,_), (_,x,_), (_,b,_))
            ((_,_,_), (l,y,r), (_,_,_)) = t + b + l + r - ((x+y) / 2)
 
 
+stencil2D2Ref
+    :: (Floating a, IArray UArray a)
+    => UArray (Int,Int) a
+    -> UArray (Int,Int) a
+    -> UArray (Int,Int) a
+stencil2D2Ref xs ys = array sh [(ix, f ix) | ix <- range sh]
+  where
+    (_,(n,m))   = bounds xs
+    (_,(u,v))   = bounds ys
+    sh          = ((0,0), (n `min` u, m `min` v))
+
+    -- boundary conditions are placed on the *source* arrays
+    --
+    get1 (x,y)  = xs IArray.! (mirror n x, mirror m y)
+    get0 (x,y)  = ys IArray.! (wrap   u x, wrap   v y)
+
+    mirror sz i
+      | i < 0     = -i
+      | i > sz    = sz - (i-sz)
+      | otherwise = i
+
+    wrap sz i
+      | i < 0     = sz + i + 1
+      | i > sz    = i - sz - 1
+      | otherwise = i
+
+    f (ix,iy) =
+      let t     = get1 (ix-1, iy  )
+          b     = get1 (ix+1, iy  )
+          x     = get1 (ix,   iy  )
+          l     = get0 (ix,   iy-1)
+          r     = get0 (ix,   iy+1)
+          y     = get0 (ix,   iy  )
+      in
+      t + b + l + r - ((x+y) / 2)
+
+
 test_stencil2_2D :: Int -> IO (() -> UArray (Int,Int) Float, () -> Acc (Array DIM2 Float))
 test_stencil2_2D n2 = withSystemRandom $ \gen -> do
   let n = round $ sqrt (fromIntegral n2 :: Double)
@@ -32,36 +69,8 @@ test_stencil2_2D n2 = withSystemRandom $ \gen -> do
   return (\() -> run_ref m1 m2, \() -> run_acc m1' m2')
   where
     run_acc xs ys = stencil2 stencil2D2 Mirror (use xs) Wrap (use ys)
-    run_ref xs ys =
-      let (_,(n,m)) = bounds xs
-          (_,(u,v)) = bounds ys
-          sh        = ((0,0), (n `min` u, m `min` v))
+    run_ref xs ys = stencil2D2Ref xs ys
 
-          -- boundary conditions are placed on the *source* arrays
-          --
-          get1 (x,y) = xs IArray.! (mirror n x, mirror m y)
-          get2 (x,y) = ys IArray.! (wrap   u x, wrap   v y)
-
-          mirror sz i
-            | i < 0     = -i
-            | i > sz    = sz - (i-sz)
-            | otherwise = i
-
-          wrap sz i
-            | i < 0     = sz + i + 1
-            | i > sz    = i - sz - 1
-            | otherwise = i
-
-          f (ix,iy) = let t = get1 (ix,  iy-1)
-                          b = get1 (ix,  iy+1)
-                          x = get1 (ix,  iy)
-                          l = get2 (ix-1,iy)
-                          r = get2 (ix+1,iy)
-                          y = get2 (ix,  iy)
-                    in
-                    t + b + l + r - ((x+y) / 2)
-      in
-      array sh [(ix, f ix) | ix <- range sh]
 
 varUse :: (Acc (Array DIM2 Int), Acc (Array DIM2 Float), Acc (Array DIM2 Float))
 varUse = (first, both, second)
