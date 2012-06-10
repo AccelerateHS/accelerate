85f1376d400e9cd052ae74d476975a907dff3a51
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2012-06-10 23:50:13 +1000

    examples: integral image to read from .bmp

--- examples/tests/image-processing/IntegralImage.hs
+++ examples/tests/image-processing/IntegralImage.hs
@@ -2,48 +2,39 @@
 
 module IntegralImage where
 
-import PGM
-
-import Data.Array.Accelerate as Acc
+import Data.Array.Accelerate    as A
+import Data.Array.Accelerate.IO as A
 
 
 -- |The value of each element in an integral image is the sum of all input elements
 -- above and to the left, inclusive. It is calculated by performing an inclusive/post
 -- scan from left-to-right then top-to-bottom.
 --
-integralImage :: (Elt a, IsNum a) => Array DIM2 a -> Acc (Array DIM2 a)
+integralImage :: (Elt a, IsFloating a) => Array DIM2 Word32 -> Acc (Array DIM2 a)
 integralImage img = sumTable
   where
     -- scan rows
-    rowArr  = reshape (lift $ Z:.(w * h)) arr
-    rowSegs = Acc.replicate (lift $ Z:.h) $ unit w
-    rowSum  = reshape (lift (Z:.w:.h)) $ Acc.scanl1Seg (+) rowArr rowSegs
+    rowArr  = reshape (index1 (w * h))  $ arr
+    rowSegs = A.replicate (index1 h)    $ unit w
+    rowSum  = reshape (index2 w h)      $ scanl1Seg (+) rowArr rowSegs
 
     -- scan cols
-    colArr  = reshape (lift $ Z:.(h * w)) $ transpose2D rowSum
-    colSegs = Acc.replicate (lift $ Z:.w) $ unit h
-    colSum  = reshape (lift (Z:.h:.w)) $ Acc.scanl1Seg (+) colArr colSegs
+    colArr  = reshape (index1 (h * w))  $ transpose rowSum
+    colSegs = A.replicate (index1 w)    $ unit h
+    colSum  = reshape (index2 h w)      $ A.scanl1Seg (+) colArr colSegs
 
     -- transpose back
-    sumTable = transpose2D colSum
+    sumTable = transpose colSum
 
     --
-    arr     = use img
+    arr     = A.map luminanceOfRGBA32 (use img)
     Z:.w:.h = unlift $ shape arr
-
-
--- |Simple 2D matrix transpose.
---
-transpose2D :: Elt a => Acc (Array DIM2 a) -> Acc (Array DIM2 a)
-transpose2D arr = backpermute (swap $ shape arr) swap arr
-  where
-    swap = lift1 $ \(Z:.x:.y) -> Z:.y:.x :: Z :. Exp Int :. Exp Int
 
 
 -- Run integralImage over the input PGM
 --
 run :: FilePath -> IO (() -> Acc (Array DIM2 Float))
 run file = do
-  pgm <- readPGM file
-  return (\() -> integralImage pgm)
+  bmp <- either (error . show) id `fmap` readImageFromBMP file
+  return (\() -> integralImage bmp)
 
