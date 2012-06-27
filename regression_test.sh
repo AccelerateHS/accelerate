74697356328021d95b3ae31579ae8d86c2bc057f
Ryan Newton <rrnewton@gmail.com>
2012-06-27 11:31:20 -0400

    Add other applications to regression_test.sh

--- regression_test.sh
+++ regression_test.sh
@@ -11,7 +11,7 @@ set -e
 echo; echo "================================================================================"
 echo "First the main battery of tests:"
 echo "================================================================================"
-./dist/build/accelerate-examples/accelerate-examples -k --size=100
+./dist/build/accelerate-examples/accelerate-examples -k --size=100 $OPTIONS
 
 echo; echo "================================================================================"
 echo "Next, additional application tests, beginning with mandelbrot:"
@@ -20,3 +20,22 @@ echo "==========================================================================
 ./dist/build/accelerate-mandelbrot/accelerate-mandelbrot --size=64 --limit=10 --benchmark --samples=1 $OPTIONS
 
 
+echo; echo "================================================================================"
+echo "accelerate-fluid:"
+echo "================================================================================"
+
+ ./dist/build/accelerate-fluid/accelerate-fluid --benchmark $OPTIONS
+
+echo; echo "================================================================================"
+echo "accelerate-crystal:"
+echo "================================================================================"
+
+ ./dist/build/accelerate-crystal/accelerate-crystal --benchmark $OPTIONS
+
+
+# echo; echo "================================================================================"
+# echo "accelerate-quickcheck:"
+# echo "================================================================================"
+
+# Apparently there isn't a flag to switch which backend to use here?
+#  ./dist/build/accelerate-quickcheck/accelerate-quickcheck 
