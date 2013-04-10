4d3f2f5ca7698b742856f7cfb59a9d695fe97e70
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2013-04-10 17:17:10 +1000

    rewrite: updates for changes to substitution module

--- Data/Array/Accelerate/Trafo/Rewrite.hs
+++ Data/Array/Accelerate/Trafo/Rewrite.hs
@@ -89,13 +89,13 @@ convertSegments = cvtA
 
       -- Things we are interested in, whoo!
       FoldSeg f z a s           -> Alet (segments s) (OpenAcc (FoldSeg (cvtF f') (cvtE z') (cvtA a') a0))
-        where f' = weakenFA f
-              z' = weakenEA z
-              a' = weakenA a
+        where f' = weakenFA rebuildOpenAcc SuccIdx f
+              z' = weakenEA rebuildOpenAcc SuccIdx z
+              a' = rebuildOpenAcc (Avar . SuccIdx) a
 
       Fold1Seg f a s            -> Alet (segments s) (OpenAcc (Fold1Seg (cvtF f') (cvtA a') a0))
-        where f' = weakenFA f
-              a' = weakenA a
+        where f' = weakenFA rebuildOpenAcc SuccIdx f
+              a' = rebuildOpenAcc (Avar . SuccIdx) a
 
 
 convertSegmentsAfun :: OpenAfun aenv t -> OpenAfun aenv t
