2cfab7593bce891587aa6441ff40590874c5ad56
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2013-05-09 18:09:04 +1000

    nofib: don't exit the program if test-framework succeeds

--- Main.hs
+++ Main.hs
@@ -2,6 +2,8 @@
 module Main where
 
 import Test.Framework
+import Control.Exception
+import System.Exit
 import System.Environment
 
 import Config
@@ -24,16 +26,27 @@ main = do
   -- segmented operations, generally depend on simpler tests.
   --
   defaultMainWithOpts
-    [ test_map conf
-    , test_zipWith conf
-    , test_foldAll conf
-    , test_fold conf
-    , test_backpermute conf
-    , test_permute conf
-    , test_prefixsum conf       -- requires fold
-    , test_foldSeg conf         -- requires scan
+    [ testGroup "prelude"
+          [ test_map conf
+          , test_zipWith conf
+          , test_foldAll conf
+          , test_fold conf
+          , test_backpermute conf
+          , test_permute conf
+          , test_prefixsum conf         -- requires fold
+          , test_foldSeg conf           -- requires scan
+          ]
+
     ]
     tfconf
+    -- test-framework wants to have a nap on success; don't let it.
+    `catch` \e -> case e of
+                    ExitSuccess -> return()
+                    _           -> throwIO e
 
-  putStrLn "MOAR THINGS!"
+  -- Now run some criterion benchmarks.
+  --
+  -- TODO: We should dump the results to file so that they can be analysed and
+  --       checked for regressions.
+  --
 
