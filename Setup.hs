f7c113cfaa53c022aa6470d6e5fb9c29834aefe5
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2013-05-12 15:41:50 +1000

    fix ArrayData instance for C types in 32-bit ghc
    * changes build type to configure so we can extract the size of various types; makes appropriate CPP macros
     * updates the ghci script to do the same
    
    fixes: AccelerateHS/accelerate#91

--- Setup.hs
+++ Setup.hs
@@ -1,4 +1,20 @@
 #! /usr/bin/env runhaskell
 
+import Control.Monad
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.Setup
+import Distribution.Simple.Utils
+import System.Directory
+
+main :: IO ()
+main = defaultMainWithHooks autoconfUserHooks { preConf = preConfHook }
+  where
+    preConfHook args flags = do
+      let verbosity = fromFlag (configVerbosity flags)
+
+      confExists <- doesFileExist "configure"
+      unless confExists $
+        rawSystemExit verbosity "autoconf" []
+
+      preConf autoconfUserHooks args flags
+
