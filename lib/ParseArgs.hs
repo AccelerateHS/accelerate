9409d4118c9622a7e95d15d3b3f0001ea543b0a4
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2013-05-23 13:01:07 +1000

    split out parseArgs': it is specific to nofib
    This avoids placing a dependency on test-framework for all example programs

--- lib/ParseArgs.hs
+++ lib/ParseArgs.hs
@@ -5,22 +5,16 @@
 
 module ParseArgs (
 
-  -- Options parsing
-  Backend(..), parseArgs, parseArgs',
+  module ParseArgs,
   module System.Console.GetOpt,
-
-  -- Evaluating expressions
-  run, run1,
 
 ) where
 
 import Data.Char
 import Data.List
 import Data.Label
-import Data.Monoid
 import System.Exit
 import System.Console.GetOpt
-import qualified Test.Framework                         as TestFramework
 import qualified Criterion.Main                         as Criterion
 import qualified Criterion.Config                       as Criterion
 
@@ -47,7 +41,9 @@ run1 CUDA        f = CUDA.run1 f
 #endif
 
 
--- | The set of backends available to execute the program
+-- | The set of backends available to execute the program. The example programs
+--   all choose 'maxBound' as the default, so there should be some honesty in
+--   how this list is sorted.
 --
 data Backend = Interpreter
 #ifdef ACCELERATE_CUDA_BACKEND
@@ -139,51 +135,4 @@ parseArgs help backend (withBackends backend -> options) config header footer (t
   (cconf, _)    <- Criterion.parseArgs Criterion.defaultConfig Criterion.defaultOptions u
 
   return (conf, cconf, non)
-
-
--- | Same as 'parseArgs', but also return options for test-framework.
---
--- Since Criterion and test-framework both bail if they encounter unrecognised
--- options, we run getOpt' ourselves. This means error messages might be a bit
--- different.
---
-parseArgs' :: (config :-> Bool)                  -- ^ access a help flag from the options structure
-           -> (config :-> Backend)               -- ^ access the chosen backend from the options structure
-           -> [OptDescr (config -> config)]      -- ^ the option descriptions
-           -> config                             -- ^ default option set
-           -> [String]                           -- ^ header text
-           -> [String]                           -- ^ footer text
-           -> [String]                           -- ^ command line arguments
-           -> IO (config, Criterion.Config, TestFramework.RunnerOptions, [String])
-parseArgs' help backend (withBackends backend -> options) config header footer (takeWhile (/= "--") -> argv) =
-  let
-      helpMsg err = concat err
-        ++ usageInfo (unlines header)                    options
-        ++ usageInfo "\nGeneric criterion options:"      Criterion.defaultOptions
-        ++ usageInfo "\nGeneric test-framework options:" TestFramework.optionsDescription
-
-  in do
-
-  -- In the first round process options for the main program. Any non-options
-  -- will be split out here so we can ignore them later. Unrecognised options
-  -- get passed to criterion and test-framework.
-  --
-  (conf,non,u)  <- case getOpt' Permute options argv of
-    (opts,n,u,[]) -> case foldr id config opts of
-      conf | False <- get help conf
-        -> putStrLn (fancyHeader backend conf header footer) >> return (conf,n,u)
-      _ -> putStrLn (helpMsg [])                             >> exitSuccess
-    --
-    (_,_,_,err) -> error (helpMsg err)
-
-  -- Test Framework
-  (tfconf, u')  <- case getOpt' Permute TestFramework.optionsDescription u of
-    (oas,_,u',[]) | Just os <- sequence oas
-                -> return (mconcat os, u')
-    (_,_,_,err) -> error (helpMsg err)
-
-  -- Criterion
-  (cconf, _)    <- Criterion.parseArgs Criterion.defaultConfig Criterion.defaultOptions u'
-
-  return (conf, cconf, tfconf, non)
 
