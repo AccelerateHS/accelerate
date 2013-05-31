5eef57718c094abe56ff53f7d98c8340518cb361
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2013-05-31 15:48:53 +1000

    nofib radixsort test

--- Test/Spectral.hs
+++ Test/Spectral.hs
@@ -9,11 +9,13 @@ import Config
 
 import Test.Framework
 import Test.Spectral.SMVM
+import Test.Spectral.RadixSort
 
 
 test_spectral :: Config -> Test
 test_spectral conf =
   testGroup "spectral"
     [ test_smvm conf
+    , test_radixsort conf
     ]
 
