aeb07347ab5f376eeb86eb63615536943b068646
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2013-08-27 15:14:21 +1000

    basic main file

---
+++ Main.hs
@@ -0,0 +1,53 @@
+
+module Main where
+
+-- friends
+import Vec3
+import Config
+import ParseArgs
+import Monitoring
+import Scene.World
+import Ray.Trace
+
+-- frenemies
+import Data.Label
+import Data.Array.Accelerate                    as A
+import Graphics.Gloss.Accelerate.Data.Color
+import Graphics.Gloss.Accelerate.Data.Picture
+
+-- library
+import System.Environment                       ( getArgs )
+import qualified Graphics.Gloss                 as G
+
+
+main :: IO ()
+main = do
+  beginMonitoring
+  argv                  <- getArgs
+  (conf, cconf, rest)   <- parseArgs optHelp optBackend options defaults header footer argv
+
+  let width     = get optWidth conf
+      height    = get optHeight conf
+      fov       = get optFieldOfView conf
+      bounces   = get optBounces conf
+      backend   = get optBackend conf
+
+      ambient   = rawColor 0.3 0.3 0.3 1.0
+
+      eye       = makeVec3 50 (-100) (-700)
+      eyePos    = A.fill (constant (Z :. height :. width)) eye
+      eyeDir    = castViewRays width height fov eye
+
+      time      = 1
+      objects   = use $ makeObjects time
+      lights    = use $ makeLights  time
+
+      scene     = run backend
+                $ A.map packRGBA
+                $ traceRays bounces ambient objects lights eyePos eyeDir
+
+  G.display
+      (G.InWindow "Ray" (width, height) (10, 10))
+      G.black
+      (bitmapOfArray scene True)
+
--- Ray/Trace.hs
+++ Ray/Trace.hs
@@ -25,7 +25,7 @@ castViewRays
     :: Int                              -- width of the display
     -> Int                              -- height
     -> Int                              -- field of view
-    -> Position                         -- eye position
+    -> Exp Position                     -- eye position
     -> Acc (Array DIM2 Direction)       -- all rays originating from the eye position
 castViewRays sizeX sizeY fov eyePos
   = let
@@ -38,7 +38,7 @@ castViewRays sizeX sizeY fov eyePos
     in
     A.generate (constant (Z :. sizeY :. sizeX))
                (\ix -> let (x, y) = xyOfPoint $ pointOfIndex sizeX sizeY ix
-                       in  normalise $ makeVec3 (x * fovX) (-y * fovY) 0 - constant eyePos)
+                       in  normalise $ makeVec3 (x * fovX) (-y * fovY) 0 - eyePos)
 
 
 -- | Cast rays into the scene
