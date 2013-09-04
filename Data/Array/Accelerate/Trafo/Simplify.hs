eea93c95fac1d3ca0092b08e480d038f698e8506
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2013-09-04 21:46:19 +1000

    simpl: get a minimal set of shape intersections

--- Data/Array/Accelerate/Trafo/Simplify.hs
+++ Data/Array/Accelerate/Trafo/Simplify.hs
@@ -24,6 +24,7 @@ module Data.Array.Accelerate.Trafo.Simplify (
 
 -- standard library
 import Prelude                                          hiding ( exp, iterate )
+import Data.List                                        ( nubBy )
 import Data.Maybe
 import Data.Monoid
 import Data.Typeable
@@ -227,16 +228,26 @@ simplifyOpenExp env = first getAny . cvtE
     cvtE' :: Gamma acc env' env' aenv -> PreOpenExp acc env' aenv e' -> (Any, PreOpenExp acc env' aenv e')
     cvtE' env' = first Any . simplifyOpenExp env'
 
-    -- If the head terms of a shape intersection match, avoid the intersection
-    -- test and return the shape.
+    -- Return the minimal set of unique shapes to intersect. This is a bit
+    -- inefficient, but the number of shapes is expected to be small so should
+    -- be fine in practice.
     --
     intersect :: Shape t
               => (Any, PreOpenExp acc env aenv t)
               -> (Any, PreOpenExp acc env aenv t)
               -> (Any, PreOpenExp acc env aenv t)
-    intersect sh1@(_,sh1') sh2@(_,sh2')
-      | Just REFL <- match sh1' sh2' = Stats.ruleFired "intersect" (yes sh1')
-      | otherwise                    = Intersect <$> sh1 <*> sh2
+    intersect (c1, sh1) (c2, sh2)
+      | getAny changed  = Stats.ruleFired "intersect" result
+      | otherwise       = result
+      where
+        sh              = leaves sh1 ++ leaves sh2
+        sh'             = nubBy (\x y -> isJust (match x y)) sh
+        changed         = Any (length sh /= length sh') <> c1 <> c2
+        result          = (changed, foldl1 Intersect sh')
+
+        leaves :: Shape t => PreOpenExp acc env aenv t -> [PreOpenExp acc env aenv t]
+        leaves (Intersect x y)  = leaves x ++ leaves y
+        leaves rest             = [rest]
 
     -- Simplify conditional expressions, in particular by eliminating branches
     -- when the predicate is a known constant.
