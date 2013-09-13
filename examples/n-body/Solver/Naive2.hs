faaa67ae4d9f69162adce579c91dee8e582425d4
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2013-09-13 18:54:22 +1000

    n-body: add a naive solver using sequential reduction

---
+++ examples/n-body/Solver/Naive2.hs
@@ -0,0 +1,25 @@
+
+module Solver.Naive2
+  where
+
+import Common.Type
+import Common.Body
+import Common.Util
+
+import Data.Array.Accelerate                    as A
+
+
+-- | Calculate accelerations on these particles in a naïve O(n^2) way.
+--
+--   This maps a _sequential_ reduction to get the total contribution for this
+--   body from all other bodies in the system.
+--
+calcAccels :: Exp R -> Acc (Vector Body) -> Acc (Vector Accel)
+calcAccels epsilon bodies
+  = let move body       = A.sfoldl (\acc next -> acc .+. accel epsilon body next)
+                                   (vec 0)
+                                   (constant Z)
+                                   bodies
+    in
+    A.map move bodies
+
