87eace46974201b89d6862a43e80b13ee23c01d1
Ryan Newton <rrnewton@gmail.com>
2014-02-12 16:45:00 -0500

    script changes

--- .jenkins_script.sh
+++ .jenkins_script.sh
@@ -7,9 +7,15 @@ cabal --version
 cabal sandbox init
 
 # First, let's make sure everything installs:
-cabal install --enable-tests \
-   ./ ./accelerate-multidev/ \
+cabal install  ./ ./accelerate-multidev/ \
    ./accelerate-backend-kit/backend-kit \
    ./accelerate-backend-kit/icc-opencl $*
 # ./accelerate-cuda/
 
+TOP=`pwd`
+for dir in accelerate-backend-kit/icc-opencl/ ; 
+do
+  cd $dir
+  cabal sandbox init --sandobx=$TOP/.cabal-sandbox/
+  cabal test
+done
