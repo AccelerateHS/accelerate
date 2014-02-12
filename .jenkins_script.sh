412122f7abcc2896d80b791a0803e4b9b11bb181
Ryan Newton <rrnewton@gmail.com>
2014-02-12 18:01:43 -0500

    Script tweak

--- .jenkins_script.sh
+++ .jenkins_script.sh
@@ -15,13 +15,15 @@ PKGS=" ./accelerate-backend-kit/backend-kit \
 cabal install --only-dependencies --enable-tests --disable-library-profiling  --disable-documentation ./  $PKGS $*
 cabal install --disable-library-profiling --disable-documentation ./  $PKGS $*
 
+TOP=`pwd`
+
 function test_dir() {
   dir=$1
   cd $dir
   cabal sandbox init --sandbox=$TOP/.cabal-sandbox/
   cabal test --show-details=always
+  cd $TOP
 }
 
-TOP=`pwd`
-test_dir ./accelerate-backend-kit/icc-opencl/
-test_dir ./accelerate-multidev/ || echo "acclerate-multidev failed tests!  But that's allowed for now."
+test_dir $TOP/accelerate-backend-kit/icc-opencl/
+test_dir $TOP/accelerate-multidev/ || echo "acclerate-multidev failed tests!  But that's allowed for now."
