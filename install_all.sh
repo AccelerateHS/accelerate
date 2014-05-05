16b044fa45021e073ceb7547f4baa410bb50079d
Joel Svensson <svenssonjoel@yahoo.se>
2014-05-05 12:43:40 -0400

    Added constraint for cuda version to install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -6,7 +6,7 @@
 # 
 PKGS="./ ./accelerate-backend-kit/backend-kit ./accelerate-backend-kit/icc-opencl ./accelerate-multidev ./accelerate-cuda "
 
-ARGS="--disable-library-profiling --disable-documentation"
+ARGS="--disable-library-profiling --disable-documentation --constraint=cuda<0.6"
 
 cabal install $ARGS $PKGS $*
 
