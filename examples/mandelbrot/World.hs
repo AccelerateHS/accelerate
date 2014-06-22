f06ec38fd4eb2cea32fe3b6e9d4570c85f041ee2
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2014-06-22 15:19:32 +1000

    mandel: load preset scene with numbers 0-9
    * Presets stolen from Repa, but currently we don't support configurable escape conditions, so some of the scenes are effectively blank.

--- examples/mandelbrot/World.hs
+++ examples/mandelbrot/World.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE GADTs         #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators       #-}
 
 module World (
 
@@ -143,6 +144,7 @@ react opt event world
     char 'x'            = toggle zooming Out
     char 'd'            = precision Double
     char 'f'            = precision Float
+    char n | isDigit n  = preset n
     char _              = const id
 
     special KeyUp       = toggle vertical Fwd
@@ -156,6 +158,9 @@ react opt event world
 
     precision f Down    = setPrecisionOfWorld f opt
     precision _ _       = id
+
+    preset n Down       = loadPreset (read [n])
+    preset _ Up         = id
 
 
 -- Miscellaneous
@@ -172,4 +177,28 @@ vertical = lens (\(World _ _ _ _ v) -> v) (\f (World p r z h v) -> World p r z h
 
 convertView :: (Real a, Fractional b) => View a -> View b
 convertView (x,y,x',y') = (realToFrac x, realToFrac y, realToFrac x', realToFrac y')
+
+-- Presets
+-- -------
+
+loadPreset :: Int -> World -> World
+loadPreset n (World _ r z h v) = load (table P.!! n)
+  where
+    load (posX, posY, zoom, _iters :: Double, _radius :: Double)        -- type signature only to suppress a warning
+      = let x' = zoom / 2
+            y' = x' * 0.75
+        in  World (posX-x', posY-y', posX+x', posY+y') r z h v
+
+    table =
+      [ (-0.7,                   0,                               3.0769,                 100,                 2)
+      , (0.20508818500545423,    0.9014915666351141   * 900/1440, 6.375321937544527e-6,   629.3354966759534,   16.0)
+      , (0.4510757067879078,     0.6144133202705898   * 900/1440, 7.632248223018773e-5,   253.61352386150395,  2.0)
+      , (0.3469337523117071,     0.6866350870407725   * 900/1440, 3.508380713647269e-5,   168.61054759193718,  1024.0)
+      , (-0.7902001921590814,    0.24910667566731381  * 900/1440, 5.071115028132377e-4,   1176.757810813391,   3.4359738368e10)
+      , (2.3127178455019423e-2, -1.301205470975472    * 900/1440, 3.6349313304610088e-9,  343.0390372557315,   2.0)
+      , (2.3127176148480418e-2, -1.3012054707668765   * 900/1440, 2.71444790387451e-10,   604.1620768089155,   2.0)
+      , (2.3127176156746785e-2, -1.301205470242045    * 900/1440, 4.49615119202067e-12,   1731.8575629678642,  2.0)
+      , (0.2550376327692795,     8.962363618058007e-4 * 900/1440, 7.351698819132829e-5,   1412.1093729760698,  16.0)
+      , (0.25498593633806477,    8.726424280526077e-4 * 900/1440, 1.6858526052251987e-10, 10492.090844482025,  2.0)
+      ]
 
