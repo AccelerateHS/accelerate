16831215437959c8f1199d67b32381036f8da3aa
Ryan Newton <rrnewton@gmail.com>
2014-07-01 11:08:06 -0400

    Various changes to testing script

--- .jenkins_script.sh
+++ .jenkins_script.sh
@@ -6,29 +6,62 @@ set -e
 PKGS=" ./ ./accelerate-backend-kit/backend-kit \
        ./accelerate-backend-kit/icc-opencl "
 
+PKGNAMES=" accelerate accelerate-cuda accelerate-backend-kit accelerate-icc-opencl "
+
 # Temporarily removing these. 
 # multidev is a bit outdated (Iterate) 
 #       ./accelerate-multidev/ " 
 #       ./accelerate-cuda/ "
 # 
 
-CBLARGS="--disable-library-profiling  --disable-documentation --constraint=cuda<0.6 $*"
+#------------------------------------------------------------
+# Choose commands 
+
+if [ "$CABAL" == "" ]; then 
+  CABAL=cabal
+fi 
+if [ "$JENKINS_GHC" == "" ]; then 
+  GHC=ghc
+  GHCPKG=ghc-pkg
+else
+  ENVSCRIPT=$HOME/rn_jenkins_scripts/acquire_ghc.sh
+  # This is specific to our testing setup at IU:
+  if [ -f "$ENVSCRIPT" ]; then 
+    source "$ENVSCRIPT"
+  fi
+  GHC=ghc-$JENKINS_GHC
+  GHCPKG=ghc-pkg-$JENKINS_GHC
+fi
+
+CBLARGS="--disable-library-profiling  --disable-documentation --constraint=cuda<0.6 --with-ghc=$GHC $*"
 
 #------------------------------------------------------------
 # Init sandbox and link it
 
 #temporarily added by js
-cabal update
 
-cabal --version
-cabal sandbox init
-
-cabal sandbox hc-pkg list
+# cabal update
+if [ "$NOSANDBOX" == "" ] || [ "$NOSANDBOX" == "0" ]; then
+  $CABAL --version
+  $CABAL sandbox init
+  $CABAL sandbox hc-pkg list
+else 
+  $CABAL sandbox delete || echo ok 
+  # rm -rf .cabal-sandbox cabal.sandbox.config
+  # Very aggressively delete sandboxes:
+  find . -name cabal.sandbox.config | xargs rm -f 
+  find . -name .cabal-sandbox       | xargs rm -rf
+  for $name in $PKGNAMES; do
+    $GHCPKG unregister $name --force || echo ok
+  done
+fi
 
 TOP=`pwd`
 for dir in $PKGS; do
   cd $dir
-  cabal sandbox init --sandbox=$TOP/.cabal-sandbox/
+  if [ "$NOSANDBOX" == "" ] || [ "$NOSANDBOX" == "0" ]; then
+    $CABAL sandbox init --sandbox=$TOP/.cabal-sandbox/
+  fi
   cd $TOP
 done
 
@@ -36,8 +69,8 @@ done
 # Begin installation
 
 # First, let's make sure everything installs:
-cabal install $CBLARGS $PKGS --force-reinstalls -j 
-cabal install $CBLARGS $PKGS --only-dependencies --enable-tests -j
+$CABAL install $CBLARGS $PKGS --force-reinstalls -j 
+$CABAL install $CBLARGS $PKGS --only-dependencies --enable-tests -j
 
 #------------------------------------------------------------
 # Begin testing
@@ -47,12 +80,12 @@ function test_dir() {
   shift
   args=$*
   cd $dir
-  time cabal test --show-details=always $args
+  time $CABAL test --show-details=always $args
   cd $TOP
 }
 
 # Provide a way to run this script for installs only without actually running the tests.
-if [ "$ACCELERATE_INSTALL_ONLY" != "1" ]; then 
+if [ "$ACCELERATE_INSTALL_ONLY" != "1" ] && [ "$NOTEST" != "1" ]; then 
 
   # (1) Test the interpreters:
   test_dir $TOP/accelerate-backend-kit/backend-kit --test-option="--threads=8"
