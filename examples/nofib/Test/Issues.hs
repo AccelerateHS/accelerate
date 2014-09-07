53287894b30713fcda2fb37bcd3f68a6578e9bee
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2014-09-07 16:46:35 +0200

    nofib: add test for AccelerateHS/accelerate#102

--- examples/nofib/Test/Issues.hs
+++ examples/nofib/Test/Issues.hs
@@ -9,6 +9,7 @@ import Config
 
 import Test.Framework
 import Test.Issues.Issue93
+import Test.Issues.Issue102
 import Test.Issues.Issue114
 import Test.Issues.Issue137
 import Test.Issues.Issue168
@@ -22,6 +23,7 @@ test_issues conf =
   testGroup "issues"
     [
       test_issue93  conf
+    , test_issue102 conf
     , test_issue114 conf
     , test_issue137 conf
     , test_issue168 conf
