b1c0908a0ebc45f8add35ed0472394ac9dd04397
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2014-09-16 23:20:49 -0400

    deprecation warning

--- examples/icebox/tests/simple/SMVM/MatrixMarket.hs
+++ examples/icebox/tests/simple/SMVM/MatrixMarket.hs
@@ -5,7 +5,7 @@ module SMVM.MatrixMarket (Matrix(..), readMatrix) where
 import Control.Applicative                      hiding ( many )
 
 import Data.Complex
-import Data.Attoparsec.Char8
+import Data.Attoparsec.ByteString.Char8
 import Data.ByteString.Lex.Double
 import qualified Data.Attoparsec.Lazy           as L
 import qualified Data.ByteString.Lazy           as L
