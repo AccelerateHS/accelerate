5030f267afd543328f2bc8f6bee517de046fc340
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2014-10-06 22:52:37 -0400

    fft: update

--- examples/fft/src-acc/Main.hs
+++ examples/fft/src-acc/Main.hs
@@ -4,18 +4,17 @@ module Main where
 import Config
 import FFT
 import HighPass
-import ParseArgs
-import Monitoring
 
 import Prelude                                          as P
 import Data.Label
-import Criterion.Main                                   ( defaultMainWith, bench, whnf )
 import System.Exit
 import System.Environment
 import System.FilePath
 
 import Data.Array.Accelerate                            as A
 import Data.Array.Accelerate.IO                         as A
+import Data.Array.Accelerate.Examples.Internal          as A
+
 
 -- Main routine ----------------------------------------------------------------
 
@@ -24,10 +23,10 @@ main
   = do
         beginMonitoring
         argv                    <- getArgs
-        (conf, cconf, rest)     <- parseArgs configHelp configBackend options defaults header footer argv
+        (conf, opts, rest)     <- parseArgs options defaults header footer argv
         (fileIn, fileOut)       <- case rest of
           (i:o:_) -> return (i,o)
-          _       -> parseArgs configHelp configBackend options defaults header footer ("--help":argv)
+          _       -> parseArgs options defaults header footer ("--help":argv)
                   >> exitSuccess
 
         -- Read in the image file
@@ -42,24 +41,20 @@ main
 
             cutoff      = get configCutoff conf
             clip        = get configClip conf
-            backend     = get configBackend conf
+            backend     = get optBackend opts
 
-        if P.not (get configBenchmark conf)
-           then do
-             -- Write out the images to file
-             --
-             let highpass       = run backend $ highpassFFT width height cutoff (use img)
-                 (mag, phase)   = run backend $ imageFFT    width height clip   (use img)
+            -- Write out the images to file
+            --
+            highpass     = run backend $ highpassFFT width height cutoff (use img)
+            (mag, phase) = run backend $ imageFFT    width height clip   (use img)
 
-             writeImageToBMP fileHP    highpass
-             writeImageToBMP fileMag   mag
-             writeImageToBMP filePhase phase
+        writeImageToBMP fileHP    highpass
+        writeImageToBMP fileMag   mag
+        writeImageToBMP filePhase phase
 
-           else do
-             -- Run the operations through criterion
-             --
-             withArgs (P.drop 2 rest) $ defaultMainWith cconf
-               [ bench "highpass" $ whnf (run1 backend (highpassFFT width height cutoff)) img
-               , bench "fft"      $ whnf (run1 backend (imageFFT    width height clip))   img
-               ]
+        runBenchmarks opts (P.drop 2 rest)
+          [ bench "highpass" $ whnf (run1 backend (highpassFFT width height cutoff)) img
+          , bench "fft"      $ whnf (run1 backend (imageFFT    width height clip))   img
+          ]
+
 
