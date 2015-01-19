8d665f7755261099d2d17c0d535af1f3779b4bea
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2015-01-19 12:42:11 -0500

    wibble

--- Data/Array/Accelerate/Trafo/Base.hs
+++ Data/Array/Accelerate/Trafo/Base.hs
@@ -60,7 +60,7 @@ import Data.Array.Accelerate.Error
 import Data.Array.Accelerate.Pretty.Print
 import Data.Array.Accelerate.Trafo.Substitution
 
-import Data.Array.Accelerate.Debug as Stats
+import Data.Array.Accelerate.Debug                      as Stats
 
 
 -- Toolkit
