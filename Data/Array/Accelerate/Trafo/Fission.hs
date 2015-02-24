694e5b1e1c6aea3c30e3c9d2b05435871e599d76
Mike Vollmer <mike@recurial.com>
2015-02-24 12:40:33 -0500

    Added trace to Fission

--- Data/Array/Accelerate/Trafo/Fission.hs
+++ Data/Array/Accelerate/Trafo/Fission.hs
@@ -37,6 +37,7 @@ import Data.Array.Accelerate.Type
 import Unsafe.Coerce
 import System.Environment
 import System.IO.Unsafe
+import Debug.Trace
 
 -- | Apply the fission transformation to a closed de Bruijn AST
 --
@@ -66,11 +67,11 @@ convertOpenAcc p@Delayed{} = p
 
 convertOpenAcc (Manifest pacc)
   = case unsafePerformIO $ lookupEnv "FISSION" of
-     Nothing  -> Manifest pacc
-     Just "0" -> Manifest pacc
-     Just "2" -> convertOpenAcc2 (Manifest pacc)
-     Just "4" -> convertOpenAcc4 (Manifest pacc)
-     Just _   -> convertOpenAcc2 (Manifest pacc)
+     Nothing  -> trace ("FISSION OFF") $ Manifest pacc
+     Just "0" -> trace ("FISSION OFF") $ Manifest pacc
+     Just "2" -> trace ("FISSION ON") $ convertOpenAcc2 (Manifest pacc)
+     Just "4" -> trace ("FISSION ON") $ convertOpenAcc4 (Manifest pacc)
+     Just _   -> trace ("FISSION ON") $ convertOpenAcc2 (Manifest pacc)
 
 convertOpenAcc2
     :: forall aenv arrs. Arrays arrs
