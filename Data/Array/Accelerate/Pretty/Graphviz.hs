98a94cc57c9ab2c59d20df5a84f6466580397041
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2016-01-25 12:09:15 -0500

    add language pragma (warning police)

--- Data/Array/Accelerate/Pretty/Graphviz.hs
+++ Data/Array/Accelerate/Pretty/Graphviz.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE GADTs                #-}
 {-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE PatternGuards        #-}
 {-# LANGUAGE RankNTypes           #-}
 {-# LANGUAGE RecordWildCards      #-}
 {-# LANGUAGE ScopedTypeVariables  #-}
