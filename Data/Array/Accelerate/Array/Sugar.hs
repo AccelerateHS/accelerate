977e225b013424c0adb1105f66563ec16ee274f8
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2016-09-03 21:38:42 +1000

    Only enable specialised array show instances for ghc-7.10+
    For earlier versions of GHC, _client_ modules would need to add the IncoherentInstances pragma.

--- Data/Array/Accelerate/Array/Sugar.hs
+++ Data/Array/Accelerate/Array/Sugar.hs
@@ -12,7 +12,6 @@
 {-# LANGUAGE TypeOperators         #-}
 {-# LANGUAGE UndecidableInstances  #-}
 #if __GLASGOW_HASKELL__ <= 708
-{-# LANGUAGE OverlappingInstances  #-}
 {-# OPTIONS_GHC -fno-warn-unrecognised-pragmas #-}
 #endif
 {-# OPTIONS_HADDOCK hide #-}
@@ -1180,8 +1179,10 @@ instance Elt e => IsList (Vector e) where
   fromListN n xs = fromList (Z:.n) xs
   fromList xs    = GHC.fromListN (length xs) xs
 
--- Convert an array to a string, with specialised instances for dimensions zero,
--- one, and two.
+#if __GLASGOW_HASKELL__ >= 710
+-- Convert an array to a string, using specialised instances for dimensions
+-- zero, one, and two. These are available for ghc-7.10 and later only (earlier
+-- versions of ghc would require -XIncoherentInstances in the client module).
 --
 -- TODO:
 --   * Make special formatting optional? It is more difficult to copy/paste the
@@ -1215,13 +1216,14 @@ instance Show (Array DIM2 e) where
           extra = 1
           width = maximum . map length
           pad w = map (\x -> replicate (w - length x + extra) ' ' ++ x)
+#endif
 
 -- This is a bit unfortunate, but we need to use an INCOHERENT instance because
 -- GHC can't determine that with the above specialisations, a DIM3+ instance
 -- covers all remaining possibilities, and lacking a general instance is
 -- problematic for operations which want a 'Show (Array sh e)' constraint.
 -- Furthermore, those clients are likely to pick this instance, rather than the
--- more specific ones above.
+-- more specific ones above, which is (perhaps) a little unfortunate.
 --
 instance {-# INCOHERENT #-} Show (Array sh e) where
   show arr@Array{}
