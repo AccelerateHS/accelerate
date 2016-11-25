2415269effadec921eb8802dee5f5ef2f287e702
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2016-11-25 19:35:32 +1100

    nofib: fix permute test

--- examples/nofib/Test/Prelude/Permute.hs
+++ examples/nofib/Test/Prelude/Permute.hs
@@ -97,7 +97,7 @@ test_permute backend opt = testGroup "permute" $ catMaybes
     --
     test_histogram :: (P.Num e, A.Num e, Similar e, IArray UArray e) => (Exp e -> Exp Int) -> (e -> Int) -> Vector e -> Property
     test_histogram f g xs =
-      forAll (sized return) $ \n ->
+      forAll arbitrary $ \(Positive n) ->
         run2 backend (histogramAcc f) (scalar n) xs ~?= histogramRef n g xs
 
     histogramAcc :: A.Num e => (Exp e -> Exp Int) -> Acc (Scalar Int) -> Acc (Vector e) -> Acc (Vector e)
