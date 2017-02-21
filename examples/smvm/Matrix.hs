280b10cc299b27c5bac79319538d37e9ed605112
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2017-02-21 12:11:42 +1100

    smvm: speed up matrix loading

--- examples/smvm/Matrix.hs
+++ examples/smvm/Matrix.hs
@@ -6,15 +6,15 @@ module Matrix (
 
 ) where
 
-import Control.Monad
 import Data.Int
-import Data.List
 import Data.Matrix.MatrixMarket                                     ( Matrix(..), Structure(..) )
 import Data.Scientific
 import Data.Vector.Storable                                         ( Vector, Storable )
+import System.IO.Unsafe
 import System.Random.MWC
 import qualified Data.Vector.Storable                               as S
-
+import qualified Data.Vector.Unboxed                                as U
+import qualified Data.Vector.Algorithms.Intro                       as V
 
 
 data CSRMatrix a =
@@ -49,13 +49,14 @@ matrixToCSR
     :: GenIO
     -> Matrix Scientific
     -> IO (CSRMatrix Double)
-matrixToCSR _ (RMatrix dim _ structure entries)
+matrixToCSR _ (RMatrix dim n structure entries)
   = return
-  $ toCSR dim
   $ case structure of
-      General  -> flip map entries
+      General  -> toCSR dim n
+                $ flip map entries
                 $ \(r,c,v) -> (fromIntegral (r-1), fromIntegral (c-1), toRealFloat v)
-      _        -> flip concatMap entries
+      _        -> toCSR dim (n*2)
+                $ flip concatMap entries
                 $ \(r,c,v) -> let v' = toRealFloat v
                                   r' = fromIntegral (r-1)
                                   c' = fromIntegral (c-1)
@@ -63,17 +64,18 @@ matrixToCSR _ (RMatrix dim _ structure entries)
                               if r' == c' then [(r',c',v')]
                                           else [(r',c',v'), (c',r',v')]
 
-matrixToCSR gen (PatternMatrix dim _ structure entries)
-  = fmap (toCSR dim)
-  $ case structure of
-      General -> forM entries
+matrixToCSR gen (PatternMatrix dim n structure entries)
+  = case structure of
+      General -> fmap (toCSR dim n)
+               $ forM' entries
                $ \(r,c) -> let r' = fromIntegral (r-1)
                                c' = fromIntegral (c-1)
                            in do
                              v <- uniform gen
                              return (r', c', v)
-      _       -> fmap concat
-               $ forM entries
+      _       -> fmap (toCSR dim (n*2))
+               $ fmap concat
+               $ forM' entries
                $ \(r,c) -> let r' = fromIntegral (r-1)
                                c' = fromIntegral (c-1)
                            in do
@@ -89,24 +91,46 @@ matrixToCSR _ IntMatrix{} = error "matrixToCSR: integer matrices not supported"
 -- Convert the given list of (row index, column index, value) triples into a CSR
 -- matrix representation.
 --
-toCSR :: (Int,Int)
-      -> [(Int32,Int32,Double)]
+{-# INLINE toCSR #-}
+toCSR :: (Int,Int)                        -- matrix dimensions
+      -> Int                              -- #non-zero elements (hint)
+      -> [(Int32,Int32,Double)]           -- (row,column,value)
       -> CSRMatrix Double
-toCSR dim entries =
+toCSR dim@(r,_) n entries =
   let cmp (r1,c1,_) (r2,c2,_)
         | r1 == r2  = compare c1 c2
         | otherwise = compare r1 r2
 
-      sorted      = sortBy cmp entries    -- possibly more efficient to sort as unboxed vector
-      (rx,ix,vs)  = unzip3 sorted
-      segd        = map (fromIntegral . length) (group rx)
-      segd_len    = S.fromList segd
+      sorted      = U.create $ do
+                      x <- U.unsafeThaw (U.fromListN n entries)
+                      V.sortBy cmp x
+                      return x
+
+      (rx,ix,vs)  = U.unzip3 sorted
+      segd_len    = S.unfoldrN r (\v -> if U.null v
+                                          then Nothing
+                                          else let (h,t) = U.span (== U.unsafeHead v) v
+                                               in  Just (fromIntegral (U.length h), t)) rx
+
       segd_off    = S.scanl (+) 0 segd_len
   in
   CSRMatrix { csr_segd_length = segd_len
             , csr_segd_offset = segd_off
-            , csr_indices     = S.fromList ix
-            , csr_values      = S.fromList vs
+            , csr_indices     = S.convert ix
+            , csr_values      = S.convert vs
             , csr_dim         = dim
             }
+
+
+-- Lazier versions of things in Control.Monad
+--
+sequence' :: [IO a] -> IO [a]
+sequence' ms = foldr k (return []) ms
+    where k m m' = do { x <- m; xs <- unsafeInterleaveIO m'; return (x:xs) }
+
+mapM' :: (a -> IO b) -> [a] -> IO [b]
+mapM' f as = sequence' (map f as)
+
+forM' :: [a] -> (a -> IO b) -> IO [b]
+forM' = flip mapM'
 
