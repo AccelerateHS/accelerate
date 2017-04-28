3cad598af5029ad623b39e0fb814e5b9a8244581
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2017-04-28 21:26:45 +1000

    update to use runN

--- lib/Data/Array/Accelerate/Examples/Internal/Backend.hs
+++ lib/Data/Array/Accelerate/Examples/Internal/Backend.hs
@@ -53,33 +53,46 @@ run CUDA        = CUDA.run
 
 {-# INLINE run1 #-}
 run1 :: (Arrays a, Arrays b) => Backend -> (Acc a -> Acc b) -> a -> b
-run1 Interpreter f = Interp.run1 f
+run1 Interpreter = Interp.run1
 #ifdef ACCELERATE_LLVM_NATIVE_BACKEND
-run1 CPU         f = CPU.run1 f
+run1 CPU         = CPU.run1
 #endif
 #ifdef ACCELERATE_LLVM_PTX_BACKEND
-run1 PTX         f = PTX.run1 f
+run1 PTX         = PTX.run1
 #endif
 #ifdef ACCELERATE_CUDA_BACKEND
-run1 CUDA        f = CUDA.run1 f
+run1 CUDA        = CUDA.run1
 #endif
 
 {-# INLINE run2 #-}
 run2 :: (Arrays a, Arrays b, Arrays c) => Backend -> (Acc a -> Acc b -> Acc c) -> a -> b -> c
-run2 backend f x y = go (x,y)
-  where
-    !go = run1 backend (A.uncurry f)
+#ifdef ACCELERATE_LLVM_NATIVE_BACKEND
+run2 CPU     f = CPU.runN f
+#endif
+#ifdef ACCELERATE_LLVM_PTX_BACKEND
+run2 PTX     f = PTX.runN f
+#endif
+run2 backend f = \x y -> run1 backend (A.uncurry f) (x,y)
 
 {-# INLINE run3 #-}
 run3 :: (Arrays a, Arrays b, Arrays c, Arrays d) => Backend -> (Acc a -> Acc b -> Acc c -> Acc d) -> a -> b -> c -> d
-run3 backend f x y z = go (x,y,z)
-  where
-    !go = run1 backend (\t -> let (a,b,c) = unlift t in f a b c)
-
+#ifdef ACCELERATE_LLVM_NATIVE_BACKEND
+run3 CPU     f = CPU.runN f
+#endif
+#ifdef ACCELERATE_LLVM_PTX_BACKEND
+run3 PTX     f = PTX.runN f
+#endif
+run3 backend f = \x y z -> run1 backend (\t -> let (a,b,c) = unlift t in f a b c) (x,y,z)
 
 {-# INLINE run4 #-}
 run4 :: (Arrays a, Arrays b, Arrays c, Arrays d, Arrays e) => Backend -> (Acc a -> Acc b -> Acc c -> Acc d -> Acc e) -> a -> b -> c -> d -> e
-run4 backend f x y z w = go (x,y,z,w)
+#ifdef ACCELERATE_LLVM_NATIVE_BACKEND
+run4 CPU     f = CPU.runN f
+#endif
+#ifdef ACCELERATE_LLVM_PTX_BACKEND
+run4 PTX     f = PTX.runN f
+#endif
+run4 backend f = \x y z w -> go (x,y,z,w)
   where
     !go = run1 backend (\t -> let (a,b,c,d) = unlift t in f a b c d)
 
