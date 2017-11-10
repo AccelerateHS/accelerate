ac235c52c20f994ba1f90536b687ae68f35eae54
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2017-11-10 11:47:47 +1100

    add missing file for nofib test

---
+++ test/nofib/Main.hs
@@ -0,0 +1,18 @@
+-- |
+-- Module      : nofib-interpreter
+-- Copyright   : [2009..2017] Trevor L. McDonell
+-- License     : BSD3
+--
+-- Maintainer  : Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+--
+
+module Main where
+
+import Data.Array.Accelerate.Test.NoFib
+import Data.Array.Accelerate.Interpreter
+
+main :: IO ()
+main = nofib runN
+
