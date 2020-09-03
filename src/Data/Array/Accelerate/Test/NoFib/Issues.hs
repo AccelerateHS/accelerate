65cd6204d30c1873a2e5e745c83699815fd88f2a
Trevor L. McDonell <trevor.mcdonell@gmail.com>
2020-09-03 12:59:07 +0200

    enable test 427

--- src/Data/Array/Accelerate/Test/NoFib/Issues.hs
+++ src/Data/Array/Accelerate/Test/NoFib/Issues.hs
@@ -33,6 +33,7 @@ module Data.Array.Accelerate.Test.NoFib.Issues (
   module Data.Array.Accelerate.Test.NoFib.Issues.Issue364,
   module Data.Array.Accelerate.Test.NoFib.Issues.Issue407,
   module Data.Array.Accelerate.Test.NoFib.Issues.Issue409,
+  module Data.Array.Accelerate.Test.NoFib.Issues.Issue427,
   module Data.Array.Accelerate.Test.NoFib.Issues.Issue436,
   module Data.Array.Accelerate.Test.NoFib.Issues.Issue437,
   module Data.Array.Accelerate.Test.NoFib.Issues.Issue439,
@@ -62,6 +63,7 @@ import Data.Array.Accelerate.Test.NoFib.Issues.Issue362
 import Data.Array.Accelerate.Test.NoFib.Issues.Issue364
 import Data.Array.Accelerate.Test.NoFib.Issues.Issue407
 import Data.Array.Accelerate.Test.NoFib.Issues.Issue409
+import Data.Array.Accelerate.Test.NoFib.Issues.Issue427
 import Data.Array.Accelerate.Test.NoFib.Issues.Issue436
 import Data.Array.Accelerate.Test.NoFib.Issues.Issue437
 import Data.Array.Accelerate.Test.NoFib.Issues.Issue439
@@ -90,6 +92,7 @@ test_issues runN =
     , test_issue364 runN
     , test_issue407 runN
     , test_issue409 runN
+    , test_issue427 runN
     , test_issue436 runN
     , test_issue437 runN
     , test_issue439 runN
