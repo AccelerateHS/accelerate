b396ac3c80082dc66a18343f8d74e23c5f73ffa0
Trevor L. McDonell <trevor.mcdonell@gmail.com>
2021-01-22 17:54:35 +0100

    add liftM*

--- src/Data/Array/Accelerate/Data/Monad.hs
+++ src/Data/Array/Accelerate/Data/Monad.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RebindableSyntax #-}
 -- |
 -- Module      : Data.Array.Accelerate.Data.Monad
 -- Copyright   : [2018..2020] The Accelerate Team
@@ -17,16 +18,19 @@
 
 module Data.Array.Accelerate.Data.Monad (
 
-  Monad(..)
+  Monad(..),
+  when,
+  liftM, liftM2, liftM3, liftM4, liftM5,
 
 ) where
 
 import Data.Array.Accelerate.Data.Functor
+import Data.Array.Accelerate.Language
 
 import Data.Array.Accelerate.Sugar.Elt
 import Data.Array.Accelerate.Smart
 
-import Prelude                                                      ( flip )
+import Prelude                                                      ( Bool, flip )
 
 
 -- | The 'Monad' class is used for scalar types which can be sequenced.
@@ -76,4 +80,84 @@ class Functor m => Monad m where
   -- | Inject a value into the monadic type
   --
   return :: (Elt a, Elt (m a)) => Exp a -> Exp (m a)
+
+
+-- | Conditional execution of a monadic expression
+--
+when :: (Monad m, Elt (m ())) => Exp Bool -> Exp (m ()) -> Exp (m ())
+when p s = cond p s (return (constant ()))
+
+-- | Promote a function to a monad
+--
+liftM :: (Monad m, Elt a, Elt b, Elt (m a), Elt (m b)) => (Exp a -> Exp b) -> Exp (m a) -> Exp (m b)
+liftM f m1 = do
+  x1 <- m1
+  return (f x1)
+
+-- | Promote a function to a monad, scanning the monadic arguments from
+-- left to right.
+--
+liftM2 :: (Monad m, Elt a, Elt b, Elt c, Elt (m a), Elt (m b), Elt (m c))
+       => (Exp a -> Exp b -> Exp c)
+       -> Exp (m a)
+       -> Exp (m b)
+       -> Exp (m c)
+liftM2 f m1 m2 = do
+  x1 <- m1
+  x2 <- m2
+  return (f x1 x2)
+
+-- | Promote a function to a monad, scanning the monadic arguments from
+-- left to right (cf. 'liftM2')
+--
+liftM3 :: (Monad m, Elt a, Elt b, Elt c, Elt d, Elt (m a), Elt (m b), Elt (m c), Elt (m d))
+       => (Exp a -> Exp b -> Exp c -> Exp d)
+       -> Exp (m a)
+       -> Exp (m b)
+       -> Exp (m c)
+       -> Exp (m d)
+liftM3 f m1 m2 m3 = do
+  x1 <- m1
+  x2 <- m2
+  x3 <- m3
+  return (f x1 x2 x3)
+
+-- | Promote a function to a monad, scanning the monadic arguments from
+-- left to right (cf. 'liftM2')
+--
+liftM4 :: (Monad m, Elt a, Elt b, Elt c, Elt d, Elt e, Elt (m a), Elt (m b), Elt (m c), Elt (m d), Elt (m e))
+       => (Exp a -> Exp b -> Exp c -> Exp d -> Exp e)
+       -> Exp (m a)
+       -> Exp (m b)
+       -> Exp (m c)
+       -> Exp (m d)
+       -> Exp (m e)
+liftM4 f m1 m2 m3 m4 = do
+  x1 <- m1
+  x2 <- m2
+  x3 <- m3
+  x4 <- m4
+  return (f x1 x2 x3 x4)
+
+-- | Promote a function to a monad, scanning the monadic arguments from
+-- left to right (cf. 'liftM2')
+--
+liftM5 :: ( Monad m
+          , Elt a, Elt b, Elt c, Elt d, Elt e, Elt f
+          , Elt (m a), Elt (m b), Elt (m c), Elt (m d), Elt (m e), Elt (m f)
+          )
+       => (Exp a -> Exp b -> Exp c -> Exp d -> Exp e -> Exp f)
+       -> Exp (m a)
+       -> Exp (m b)
+       -> Exp (m c)
+       -> Exp (m d)
+       -> Exp (m e)
+       -> Exp (m f)
+liftM5 f m1 m2 m3 m4 m5 = do
+  x1 <- m1
+  x2 <- m2
+  x3 <- m3
+  x4 <- m4
+  x5 <- m5
+  return (f x1 x2 x3 x4 x5)
 
