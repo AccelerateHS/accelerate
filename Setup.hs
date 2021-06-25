915c94835cb563f829e8b40b7ba552344fba5bde
Trevor L. McDonell <trevor.mcdonell@gmail.com>
2021-06-25 13:27:00 +0200

    include tracy executables as part of accelerate build

--- Setup.hs
+++ Setup.hs
@@ -4,25 +4,30 @@ module Main where
 import Distribution.Extra.Doctest
 import Distribution.PackageDescription
 import Distribution.Simple
+import Distribution.Simple.LocalBuildInfo
 import Distribution.Simple.Setup
 import Distribution.Simple.Utils
+import Distribution.System
 import Distribution.Verbosity
 
 import Control.Monad
 import Data.Maybe
 import System.Directory
+import System.FilePath
+import Text.Printf
 
 
 main :: IO ()
 main =
   defaultMainWithHooks (doctestsUserHooks "doctest")
-    { preConf = preConfHook
+    { preConf   = preConfHook
+    , postBuild = postBuildHook
     }
 
 preConfHook :: Args -> ConfigFlags -> IO HookedBuildInfo
-preConfHook args configFlags = do
-  let verbosity = fromFlagOrDefault normal $ configVerbosity configFlags
-      debugging = fromMaybe False $ lookupFlagAssignment (mkFlagName "debug") (configConfigurationsFlags configFlags)
+preConfHook args config_flags = do
+  let verbosity = fromFlagOrDefault normal $ configVerbosity config_flags
+      debugging = fromMaybe False $ lookupFlagAssignment (mkFlagName "debug") (configConfigurationsFlags config_flags)
 
   when debugging $ do
     yes <- doesFileExist "cbits/tracy/TracyClient.cpp"
@@ -44,5 +49,45 @@ preConfHook args configFlags = do
              rawSystemExit verbosity "tar" ["-xzf", archive, "-C", "cbits/tracy", "--strip-components", "1"]
              removeFile archive
 
-  preConf simpleUserHooks args configFlags
+  preConf simpleUserHooks args config_flags
+
+postBuildHook :: Args -> BuildFlags -> PackageDescription -> LocalBuildInfo -> IO ()
+postBuildHook args build_flags pkg_desc lbi = do
+  let config_flags  = configFlags lbi
+      Platform _ os = hostPlatform lbi
+      verbosity     = fromFlagOrDefault normal $ buildVerbosity build_flags
+      debugging     = fromMaybe False $ lookupFlagAssignment (mkFlagName "debug") (configConfigurationsFlags config_flags)
+      targets       = [ ("tracy-capture", "capture",  "capture-release")
+                      , ("tracy",         "profiler", "Tracy-release") ]
+
+  when debugging $ do
+    case os of
+      Windows -> return ()  -- XXX: TODO
+      _       ->
+        forM_ targets $ \(hs_exe, c_dir, c_exe) -> do
+          let c_builddir  = "cbits/tracy" </> c_dir </> "build/unix"
+              hs_builddir = buildDir lbi </> hs_exe
+              c_tmpdir    = c_builddir </> "obj"
+              hs_tmpdir   = hs_builddir </> hs_exe ++ "-tmp"
+
+          setupMessage verbosity (printf "Building executable '%s' for" hs_exe) (package pkg_desc)
+
+          -- symlink the C build directory into the HS build directories
+          exists <- doesDirectoryExist c_tmpdir
+          unless exists $ createDirectoryLink ("../../../../.." </> hs_tmpdir) c_tmpdir
+
+          -- prevent having to re-link every time we build the library
+          executable <- doesFileExist (hs_builddir </> hs_exe)
+          when executable $ renameFile (hs_builddir </> hs_exe) (c_builddir </> c_exe)
+
+          -- build
+          rawSystemExit verbosity "make" ["-C", c_builddir]
+
+          -- move executable to the final destination
+          renameFile (c_builddir </> c_exe) (hs_builddir </> hs_exe)
+
+          -- clean up after ourselves
+          unless exists $ removeDirectoryLink c_tmpdir
+
+  postBuild simpleUserHooks args build_flags pkg_desc lbi
 
