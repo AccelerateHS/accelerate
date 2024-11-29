0b9467f46421a80b0be2a3cde7d18fbb79ed0db3
Tom Smeding <tom@tomsmeding.com>
2024-11-29 21:30:53 +0100

    Upgrade tracy to v0.11.1
    Tracy moved from Make to CMake in v0.11, so the Setup.hs machinery has
    to be updated. Fortunately, the result is simpler, but we do (of course)
    gain a dependency on CMake.

--- Setup.hs
+++ Setup.hs
@@ -43,7 +43,7 @@ preConfHook args config_flags = do
            then rawSystemExit verbosity "git" ["submodule", "update", "--init", "--recursive"]
            else do
              -- XXX: This must be kept up to date with the git submodule revision
-             let archive = "v0.9.1.tar.gz"
+             let archive = "v0.11.1.tar.gz"
              createDirectoryIfMissing True "cbits/tracy"
              rawSystemExit verbosity "curl" ["-LO", "https://github.com/wolfpld/tracy/archive/refs/tags/" ++ archive]
              rawSystemExit verbosity "tar" ["-xzf", archive, "-C", "cbits/tracy", "--strip-components", "1"]
@@ -59,61 +59,32 @@ postBuildHook args build_flags pkg_desc lbi = do
       Platform _ os = hostPlatform lbi
       verbosity     = fromFlagOrDefault normal $ buildVerbosity build_flags
       debugging     = fromMaybe False $ lookupFlagAssignment (mkFlagName "debug") (configConfigurationsFlags config_flags)
-      targets       = [ ("tracy-capture", "capture",  "capture-release")
-                      , ("tracy",         "profiler", "Tracy-release") ]
+      targets       = [ ("tracy-capture", "capture",  "tracy-capture")
+                      , ("tracy",         "profiler", "tracy-profiler") ]
 
   when debugging $ do
     case os of
       Windows -> return ()  -- XXX TODO: Windows users get the dummy executable that just throws an error
       _       ->
         forM_ targets $ \(hs_exe, c_dir, c_exe) -> do
-          let c_builddir  = "cbits/tracy" </> c_dir </> "build/unix"
+          let c_projdir  = "cbits/tracy" </> c_dir
               hs_builddir = buildDir lbi </> hs_exe
-              c_tmpdir    = c_builddir </> "obj"
               hs_tmpdir   = hs_builddir </> hs_exe ++ "-tmp"
 
-          setupMessage verbosity (printf "Building executable '%s' for" hs_exe) (package pkg_desc)
+          setupMessage verbosity (printf "Building executable '%s' from Tracy C++ sources for" hs_exe) (package pkg_desc)
 
-          -- Symlink the C build directory into the HS build directories.
-          -- We need to take care here: the symlink might already exist but point
-          -- to a nonexistent path, in which case doesDirectoryExist returns False
-          -- but it is still a symlink.
-          whenM (pathIsSymbolicLinkSafe c_tmpdir) $ removeFile c_tmpdir
-          objExists <- doesPathExist c_tmpdir
-          objExistsDir <- doesDirectoryExist c_tmpdir
-          createdObjSymlink <-
-            if | objExists && not objExistsDir ->
-                  dieNoVerbosity $ c_tmpdir ++ " is a file; remove this to continue compiling accelerate."
-               | objExistsDir ->
-                  return False
-               | otherwise -> do
-                  -- Make the symlink (relative from c_builddir, hence the 5x ..)
-                  createDirectoryLink ("../../../../.." </> hs_tmpdir) c_tmpdir
-                  -- Create that temp directory if necessary. This is required if the accelerate
-                  -- package is built as a dependency from another cabal project, because then only
-                  -- the library is built, and not the executables such as tracy-capture.
-                  createDirectoryIfMissing True hs_tmpdir
-                  return True
+          -- TODO: This creates a separate build directory for each tracy
+          -- executable (of which we build two, at the time of writing). This
+          -- means that some duplicate work is done (building capstone twice).
+          -- Could we share a build directory between the two?
 
-          -- prevent having to re-link every time we build the library
-          executable <- doesFileExist (hs_builddir </> hs_exe)
-          when executable $ renameFile (hs_builddir </> hs_exe) (c_builddir </> c_exe)
+          rawSystemExit verbosity "cmake" ["-B", hs_tmpdir, "-S", c_projdir, "-DCMAKE_BUILD_TYPE=Release"]
 
-          -- build
-          rawSystemExit verbosity "make" ["-C", c_builddir]
+          -- Build in parallel with 2 jobs because likely, accelerate is one of
+          -- the last dependencies in a build, so we aren't stealing CPU time
+          -- from other packages, and tracy takes way too long to build
+          rawSystemExit verbosity "cmake" ["--build", hs_tmpdir, "--config", "Release", "-j", "2"]
 
-          -- move executable to the final destination
-          renameFile (c_builddir </> c_exe) (hs_builddir </> hs_exe)
-
-          -- clean up after ourselves
-          when createdObjSymlink $ removeDirectoryLink c_tmpdir
+          renameFile (hs_tmpdir </> c_exe) (hs_builddir </> hs_exe)
 
   postBuild simpleUserHooks args build_flags pkg_desc lbi
-
--- | Returns True if there is a symbolic link at the given path, False otherwise.
-pathIsSymbolicLinkSafe :: FilePath -> IO Bool
-pathIsSymbolicLinkSafe fp =
-  handleDoesNotExist False $ pathIsSymbolicLink fp
-
-whenM :: IO Bool -> IO () -> IO ()
-whenM mcond action = mcond >>= \cond -> when cond action
